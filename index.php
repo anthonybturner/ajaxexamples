573280993485dcf3f26a7211a903640f2ab4a127
anthonyturner2k <anthonyturner2k@gmail.com>
2013-12-24 07:48:03 -0500

    AJAX, JSON request

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+			<title>Javascript and JSON</title>	
+	</head>
+	
+	<body>	
+		<h2> Links</h2>
+		
+		<ol id="links">
+		</ol>
+		
+		<script src="Resources/Scripts/myscript.js">
+			
+			
+		</script>
+	</body>
+</html>
